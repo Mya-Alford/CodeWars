7a15c98f5697b4db0946982ed85fb6eb237fe6dc
myaal <mya.alford1@gmail.com>
2021-09-11 20:16:58 -0400

    codewars

---
+++ warfour/main.js
@@ -0,0 +1,8 @@
+// Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages
+
+if(n < 0 || m < 0){
+    return 0
+  }
+  return n * m
+
+  // https://www.codewars.com/kata/55f9b48403f6b87a7c0000bd/train/javascript
